316d89dc6207593e282ea977ecc07044c1659905
Alexander Torres <Alexander.Torres@ssa.gov>
2025-02-07 23:35:23 -0600

    exploring data types PS course

---
+++ greeting.py
@@ -0,0 +1,10 @@
+name = 'Alex'
+print(name)
+
+store_name = "Hello Alex Store"
+print(store_name)
+
+hello = "Hello"
+name = input("What is your name?\n")
+greeting = hello + " " + name
+print(greeting)
---
+++ tax.py
@@ -0,0 +1,9 @@
+amount = 10
+tax = 0.06
+total = amount + (amount * tax)
+print(total)
+
+amount = 100
+
+total = amount + (amount * tax)
+print(total)
